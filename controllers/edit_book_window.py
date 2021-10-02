019688b235edc90d60e3dda2d0b11e0ff8dba677
Feno <diego.el.feno@gmail.com>
2021-10-02 17:12:33 -0300

    Primera version funcional

--- controllers/edit_book_window.py
+++ controllers/edit_book_window.py
@@ -1,24 +1,76 @@
-from PySide2.QtWidgets import QWidget
+from PySide2.QtWidgets import QWidget, QFileDialog
 from PySide2.QtCore import Qt
 from views.edit_book_window import EditBookForm
+from db.books import select_book_by_id, update_book
+import re
+import os
+import shutil
 
 class EditBookWindow(QWidget, EditBookForm):
     
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, _id=None):
+        self._id = _id
         super().__init__(parent)
 
         self.setupUi(self)
         self.setWindowFlag(Qt.Window)
+        self.populate_inputs()
 
     def populate_inputs(self):
-        pass
+        data = select_book_by_id(self._id)
+        
+        self.titleLineEdit.setText(data[1])
+        self.categoryLineEdit.setText(data[2])
+        self.pageQtySpinBox.setValue(data[3])
+        self.pageReadQtySpinBox.setValue(data[4])
+        self.filePathLineEdit.setText(data[5])
+        self.descriptionTextEdit.setText(data[6])
+        
 
     def select_file(self):
-        pass
+        self.old_path = self.filePathLineEdit.text()
+        file_path = QFileDialog.getOpenFileName()[0]
+        self.filePathLineEdit.setText(file_path)
 
     def check_inputs(self):
-        pass
+        title = self.titleLineEdit.text()
+        category = self.categoryLineEdit.text()
+        ##page_qty = self.pageQtySpinBox.value()
+        path = self.filePathLineEdit.text()
+
+        errors_count = 0
+
+        if title == "":
+            print("El campo titulo es obligatorio")
+            errors_count += 1
+        if category == "":
+            print("El campo categoria es obligatorio")
+            errors_count += 1
+        if path == "":
+            print("El campo ubicacion es obligatorio")
+            errors_count += 1
+
+        if errors_count == 0:
+            return True
 
     def edit_book(self):
-        pass
+        title = self.titleLineEdit.text()
+        category = self.categoryLineEdit.text()
+        page_qty = self.pageQtySpinBox.value()
+        page_qty_read = self.pageReadQtySpinBox.value()
+        path = self.filePathLineEdit.text()
+        description = self.descriptionTextEdit.toPlainText()
+
+        data = [title, category, page_qty, page_qty_read, path, description]
+
+        if self.check_inputs():
+            path_to_check = "book_files\\" + re.split("/|\\")
+            result = os.path.exists(path_to_check)
+
+            if result == False:
+                data[4] = shutil.copy(path, "book_files")
+                os.remove(self.old_path)
+
+            if update_book(self._id, data):
+                self.close()
     
--- controllers/main_window.py
+++ controllers/main_window.py
@@ -1,6 +1,7 @@
-from PySide2.QtWidgets import QWidget, QTableWidgetItem
+from PySide2.QtWidgets import QWidget, QTableWidgetItem, QAbstractItemView
 from views.main_window import ListBookForm
-from db.books import select_all_books
+from db.books import select_all_books, select_book_by_title, select_book_by_category, delete_book
+import os
 
 class ListBookWindow(QWidget, ListBookForm):
 
@@ -12,6 +13,11 @@ class ListBookWindow(QWidget, ListBookForm):
         self.editBookButton.clicked.connect(self.open_edit_book_window)
         self.table_config()
         self.populate_table(select_all_books())
+        self.populate_combobox()
+        self.refreshButton.clicked.connect(lambda: self.populate_table(select_all_books()))
+        self.openBookButton.clicked.connect(self.open_book)
+        self.searchButton.clicked.connect(self.search)
+        self.deleteBookButton.clicked.connect(self.remove_book)
 
     def open_new_book_window(self):
         from controllers.new_book_window import NewBookWindow
@@ -20,19 +26,49 @@ class ListBookWindow(QWidget, ListBookForm):
 
     def open_edit_book_window(self):
         from controllers.edit_book_window import EditBookWindow
-        window = EditBookWindow(self)
-        window.show()
+        selected_row = self.listBooksTable.selectedItems()
+
+        if selected_row:
+            book_id = int(selected_row[0].text())
+            window = EditBookWindow(self, book_id)
+            window.show()
+        
+        self.listBooksTable.clearSelection()
+
+        
 
     def open_book(self):
-        pass
+        selected_row = self.listBooksTable.selectedItems()
+
+        if selected_row:
+            path = selected_row[5].text()
+            os.startfile(path)
+        
+        self.listBooksTable.clearSelection()
 
     def remove_book(self):
-        pass
+        selected_row = self.listBooksTable.selectedItems()
+
+        if selected_row:
+            book_id = int(selected_row[0].text())
+            row = selected_row[0].row()
+
+            if delete_book(book_id):
+                file_path = selected_row[5].text()
+                self.listBooksTable.removeRow(row)
+                os.remove(file_path)
+        
+        self.records_qty()
+
+
+
 
     def table_config(self):
         comlumn_headers = ("ID", "Titulo", "Categoria", "Cantidad de paginas", "Cantidad de paginas leidas", "Ruta", "Descripcion")
         self.listBooksTable.setColumnCount(len(comlumn_headers))
         self.listBooksTable.setHorizontalHeaderLabels(comlumn_headers)
+
+        self.listBooksTable.setSelectionBehavior(QAbstractItemView.SelectRows)
 
     def populate_table(self, data):
         self.listBooksTable.setRowCount(len(data))
@@ -40,18 +76,38 @@ class ListBookWindow(QWidget, ListBookForm):
         for (index_row, row) in enumerate(data):
             for (index_cell, cell) in enumerate(row):
                 self.listBooksTable.setItem(index_row, index_cell, QTableWidgetItem(str(cell)))
+        
+        self.records_qty()
 
     def populate_combobox(self):
-        pass
+        cb_options = ("", "Titulo", "Categoria")
+        self.searchByComboBox.addItems(cb_options)
 
-    def search_book_by_title(self):
-        pass
+    def search_book_by_title(self, title):
+        data = select_book_by_title(title)
+        self.populate_table(data)
 
-    def search_book_by_category(self):
-        pass
+    def search_book_by_category(self, category):
+        data = select_book_by_category(category)
+        self.populate_table(data)
 
-    def serach(self):
-        pass
+    def search(self):
+        option_selected = self.searchByComboBox.currentText()
+        parameter = self.parameterLineEdit.text()
+
+        if option_selected == "":
+            print("Debe seleccionar una opcion")
+        else:
+            if parameter == "":
+                print("El campo de busqueda esta vacio")
+            else:
+                if option_selected == "Titulo":
+                    self.search_book_by_title(parameter)
+                elif option_selected == "Categoria":
+                    self.search_book_by_category(parameter)
+        
+        
 
     def records_qty(self):
-        pass
+        qty_rows = str(self.listBooksTable.rowCount())
+        self.booksQtyLabel.setText(qty_rows)
